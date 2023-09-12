02019d3cc7fc76c9743b9bb3ebc66f7c350e54bd
Sarah Schmoller <sarah.schmoller@gmail.com>
2023-09-12 15:52:16 -0600

    Added homepage file

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+
+import React, { useEffect, useRef, useState } from 'react';
+import '../styles/homePageStyles.css';
+
+function HomePage() {
+
+// HTML FORMATTING
+
+  return (
+    <body id='body'>
+    </body>
+  );
+}
+
+export default HomePage;
