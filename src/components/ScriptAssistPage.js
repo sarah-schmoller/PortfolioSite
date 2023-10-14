1e0fc2c30cc0d0d8ead859b1ddca088b9ef26fe9
Sarah Schmoller <sarah.schmoller@gmail.com>
2023-10-14 14:12:31 -0600

    Added paragraph on script assist project page

--- src/components/ScriptAssistPage.js
+++ src/components/ScriptAssistPage.js
@@ -23,6 +23,11 @@ function ScriptAssist() {
           </div>
           <div className="projectDescriptionContainer">
             <div className="projectDescription">
+              <span>
+                <a href="https://variety.com/2018/digital/news/xandra-voice-experiences-alexa-google-home-1202917582" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "underline" }}>
+                  Xandra, Inc.
+                </a> was a small conversation design studio based in New York between the years 2017 and 2021. It specialized in producing interactive storytelling experiences using branching narratives, which were published as skills on Amazon Alexa. During its years in operation, Xandra worked as a contractor for brands like Google, HBO, and Nickelodeon, with many projects covering major media properties such as Westworld and The Walking Dead.
+              </span>
             </div>
           </div>
         </div>
