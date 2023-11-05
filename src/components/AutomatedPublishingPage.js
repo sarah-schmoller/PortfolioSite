b0d45801f2ef01c697f06dd74b9de6e041c9fac8
Sarah Schmoller <sarah.schmoller@gmail.com>
2023-11-05 12:06:22 -0600

    Added paragraph to automated publishing project page

--- src/components/AutomatedPublishingPage.js
+++ src/components/AutomatedPublishingPage.js
@@ -21,6 +21,10 @@ function AutomatedPublishing() {
             ))}
           </div>
           <div className="projectDescriptionContainer">
+            <div className="projectDescription">
+              <span>The AI Story Autopublisher is the product I have worked on the longest as technical lead for Dow Jones’ AI Content Generation team. This represented the first of the our efforts towards automated article generation when the team was founded in 2020, and has developed into an expansive framework managing production of many different types of AI content. The system is composed of a multi-step process built on AWS lambda functions, and orchestrated by step functions. Market data is pulled from various sources and parsed; the data is fed into an AI article generation system, which outputs new story text; and finally, the text is formatted for publication and sent off for display on one of Dow Jones’ product sites.
+              </span>
+            </div>
           </div>
         </div>
       </div>
