b95bc2d0d4f6c4efde651204367e855594b76950
Sarah Schmoller <sarah.schmoller@gmail.com>
2023-11-15 18:48:42 -0600

    Added initial landing page for AI authoring engine project

---
+++ src/components/AiAuthoringEnginePage.js
@@ -0,0 +1,18 @@
+
+import React, {
+} from 'react';
+import '../styles/projectPageStyles.css';
+
+
+function AppPage() {
+
+  return (
+    <body>
+      <a href="./sample-protected.html">Redirect to Html page</a>
+      <style></style>
+    </body>
+  ); 
+}
+
+export default AppPage;
+
